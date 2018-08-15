54c60c3c88e2808304c476408df2cfb56d458733
mike stedman <barbeque@users.noreply.github.com>
2018-08-15 17:24:26 -0600

    Tests

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,13 @@ impl Double for i32 {
 fn main() {
     println!("Hello, world! {} {}", 36.double(), 36.wordify());
 }
+
+#[cfg(test)]
+mod tests {
+    use Double;
+
+    #[test]
+    fn wordify_works() {
+        assert_eq!("thirty-six", 36_i32.wordify());
+    }
+}
